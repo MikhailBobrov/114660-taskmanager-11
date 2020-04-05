33966dc226828e09ea9ca52fa39a7566a991d700
yoksel <yokselzok@gmail.com>
2020-04-05 19:24:31 +0300

    Move backtick to right place; move export to const

--- src/components/card.js
+++ src/components/card.js
@@ -55,7 +55,7 @@ export default class Card {
             </div>
           </div>
         </div>
-      </article>
-    `);
+      </article>`
+    );
   }
 }
--- src/helpers/getClass.js
+++ src/helpers/getClass.js
@@ -1,7 +1,5 @@
-const getClass = ({base, mods}) => {
+export const getClass = ({base, mods}) => {
   return mods.reduce((prev, mod) => {
     return `${prev} ${base}--${mod}`;
   }, base);
 };
-
-export {getClass};
