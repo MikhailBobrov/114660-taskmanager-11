8966ed855b73eea4775de52bde097e9d84b4055a
yoksel <yokselzok@gmail.com>
2020-04-07 22:23:01 +0300

    Rewrite Board component & main.js to use mocks

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
+import {MAX_CARDS, MAX_CARDS_SHOW} from './const.js';
+
 import Menu from './components/menu';
 import Filter from './components/filter';
 import Board from './components/board';
+
+import {getCardsData} from './mocks/cards';
+
+const cardsData = getCardsData(MAX_CARDS);
+
+import {getFilterItems} from './mocks/filter';
 
 const controlElem = document.querySelector(`.control`);
 const mainElem = document.querySelector(`.main`);
 
 const menu = new Menu();
-const filter = new Filter();
-const board = new Board();
+const filter = new Filter(getFilterItems(cardsData));
+const board = new Board(cardsData);
 
-const render = (elem, template) => {
-  elem.insertAdjacentHTML(`beforeend`, template);
+const render = (target, elem) => {
+  target.append(elem);
 };
 
-render(controlElem, menu.getTmpl());
-render(mainElem, filter.getTmpl());
-render(mainElem, board.getTmpl());
+render(controlElem, menu.render());
+render(mainElem, filter.render());
+render(mainElem, board.render());
