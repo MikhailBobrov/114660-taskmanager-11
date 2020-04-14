026ba51809768d025dff4ba33ab91f4bb9846a7a
yoksel <yokselzok@gmail.com>
2020-04-14 12:38:49 +0300

    Beautify Menu component code

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,35 +1,45 @@
 import {createElement} from '../helpers';
 
 export default class Menu {
+  constructor() {
+    this.itemsData = [
+      {
+        id: `new-task`,
+        name: `+ ADD NEW TASK`,
+      },
+      {
+        id: `task`,
+        name: `TASKS`
+      },
+      {
+        id: `statistic`,
+        name: `STATISTICS`
+      },
+    ];
+  }
+
+  getItems() {
+    return this.itemsData.reduce((prev, item) => {
+      const {id, name} = item;
+      return (
+        `${prev}<input
+          type="radio"
+          name="control"
+          id="control__${id}"
+          class="control__input visually-hidden"
+        />
+        <label
+          for="control__${id}"
+          class="control__label control__label--${id}"
+          >${name}</label>`
+      );
+    }, ``);
+  }
+
   getTmpl() {
     return (
       `<section class="control__btn-wrap">
-        <input
-          type="radio"
-          name="control"
-          id="control__new-task"
-          class="control__input visually-hidden"
-        />
-        <label for="control__new-task" class="control__label control__label--new-task"
-          >+ ADD NEW TASK</label
-        >
-        <input
-          type="radio"
-          name="control"
-          id="control__task"
-          class="control__input visually-hidden"
-          checked
-        />
-        <label for="control__task" class="control__label">TASKS</label>
-        <input
-          type="radio"
-          name="control"
-          id="control__statistic"
-          class="control__input visually-hidden"
-        />
-        <label for="control__statistic" class="control__label"
-          >STATISTICS</label
-        >
+        ${this.getItems()}
       </section>`
     );
   }
