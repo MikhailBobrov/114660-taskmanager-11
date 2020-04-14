62189c608160fbf1cb4cb945eacec8d2e1cedc94
yoksel <yokselzok@gmail.com>
2020-04-14 12:43:44 +0300

    Beautify Sort component code

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,12 +1,42 @@
 import {createElement} from '../helpers';
 
 export default class Sort {
+  constructor() {
+    this.itemsData = [
+      {
+        id: `default`,
+        name: `SORT BY DEFAULT`
+      },
+      {
+        id: `date-up`,
+        name: `SORT BY DATE up`
+      },
+      {
+        id: `date-down`,
+        name: `SORT BY DATE down`
+      },
+    ];
+  }
+
+  getItems() {
+    return this.itemsData.reduce((prev, item) => {
+      const {id, name} = item;
+      return (
+        `${prev}<a
+          href="#"
+          class="board__filter"
+          data-sort-type="${id}"
+        >
+          ${name}
+        </a>`
+      );
+    }, ``);
+  }
+
   getTmpl() {
     return (
       `<div class="board__filter-list">
-        <a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
-        <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
-        <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>
+        ${this.getItems()}
       </div>`
     );
   }
