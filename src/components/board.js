d510039168f0f5ab039a5499ab7c9562555c5bbe
yoksel <yokselzok@gmail.com>
2020-04-14 14:06:15 +0300

    Unify names in Board component

--- src/components/board.js
+++ src/components/board.js
@@ -10,14 +10,14 @@ export default class Board {
     this._cardsData = cardsData;
     this._quantityLoaded = 0;
     this._sort = new Sort();
-    this._sectionElem = this._createSectionElem();
-    this._tasksElem = this._createTasksElem();
-    this._moreBtn = this._createMoreBtnElem();
+    this._element = this._createSectionElement();
+    this._tasksElement = this._createTasksElement();
+    this._moreBtnElement = this._createMoreBtnElement();
 
     this._fillSection();
 
     this._addCards = this._addCards.bind(this);
-    this._moreBtn.addEventListener(`click`, this._addCards);
+    this._moreBtnElement.addEventListener(`click`, this._addCards);
   }
 
   _addCards() {
@@ -27,34 +27,34 @@ export default class Board {
     this._quantityLoaded = nextLoadStart;
 
     if (this._quantityLoaded >= this._cardsData.length) {
-      this._moreBtn.remove();
+      this._moreBtnElement.remove();
     }
 
     for (const data of cardsDataToShow) {
-      this._tasksElem.append(new Card(data).getElement());
+      this._tasksElement.append(new Card(data).getElement());
     }
   }
 
-  _createSectionElem() {
+  _createSectionElement() {
     return createElement(`<section class="board container"></section>`);
   }
 
-  _createTasksElem() {
+  _createTasksElement() {
     return createElement(`<div class="board__tasks"></div>`);
   }
 
-  _createMoreBtnElem() {
+  _createMoreBtnElement() {
     return createElement(`<button class="load-more" type="button">load more</button>`);
   }
 
   _fillSection() {
-    this._sectionElem.append(this._sort.getElement());
-    this._sectionElem.append(this._tasksElem);
+    this._element.append(this._sort.getElement());
+    this._element.append(this._tasksElement);
     this._addCards();
-    this._sectionElem.append(this._moreBtn);
+    this._element.append(this._moreBtnElement);
   }
 
   getElement() {
-    return this._sectionElem;
+    return this._element;
   }
 }
