a379ac7256e02a6acdad7dc7c49efc77d3e8d9b0
yoksel <yokselzok@gmail.com>
2020-04-18 14:41:55 +0300

    Add renderElement() & removeElement() helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,6 @@
 export {getDate, getTime} from './getDateTime';
 export {createElement} from './createElement';
+export {renderElement} from './renderElement';
+export {removeElement} from './removeElement';
 export {getClass} from './getClass';
 export {getRandomBool} from './getRandomBool';
