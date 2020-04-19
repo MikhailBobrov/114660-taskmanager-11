9c657947a626411238b6eb679295f745cf33dbfe
yoksel <yokselzok@gmail.com>
2020-04-19 14:59:14 +0300

    Change rendering cards in TaskContainer; add events in TaskContainer instead of Card

--- src/components/board/tasks-container.js
+++ src/components/board/tasks-container.js
@@ -1,12 +1,32 @@
 import AbstractComponent from '../abstract-component';
 import Card from '../card';
+import CardEdit from '../card-edit';
+import {renderElement} from '../../helpers';
 
 export default class TasksContainer extends AbstractComponent {
+  renderTask(element, data) {
+    const card = new Card(data);
+    data.isEdit = true;
+    const cardEdit = new CardEdit(data);
+
+    function replaceCardToEdit() {
+      card.getElement().replaceWith(cardEdit.getElement());
+    }
+    function replaceEditTocard() {
+      cardEdit.getElement().replaceWith(card.getElement());
+    }
+
+    card.setEditBtnHandler(replaceCardToEdit);
+    cardEdit.setSubmitHandler(replaceEditTocard);
+
+    renderElement(element, card);
+  }
+
   addCards(cardsData) {
     const element = this.getElement();
 
     for (const data of cardsData) {
-      element.append(new Card(data).getElement());
+      this.renderTask(element, data);
     }
   }
 
