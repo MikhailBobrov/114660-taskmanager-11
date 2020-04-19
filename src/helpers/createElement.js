81249ff2d829c1d70f90b839077264535afd0373
yoksel <yokselzok@gmail.com>
2020-04-19 14:53:42 +0300

    Now createElement() returns empty string instead of null

--- src/helpers/createElement.js
+++ src/helpers/createElement.js
@@ -1,6 +1,10 @@
 const templateElem = document.createElement(`template`);
 
 export const createElement = (str) => {
+  if (!str) {
+    return ``;
+  }
+
   templateElem.innerHTML = str;
   return templateElem.content.firstElementChild;
 };
