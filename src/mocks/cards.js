bf4bdb36512ec307c096f00cb14eb329370f6d08
yoksel <yokselzok@gmail.com>
2020-05-05 14:15:19 +0300

    Fix weekdays in card mocks

--- src/mocks/cards.js
+++ src/mocks/cards.js
@@ -23,35 +23,24 @@ const getWeekDays = (hasChecked = true) => {
     return WEEKDAYS;
   }
 
-  return Object.entries(WEEKDAYS).reduce((prev, [name]) => {
-    prev[name] = getRandomBool();
-    return prev;
-  }, {});
+  return Object.assign({}, WEEKDAYS, {mo: true});
 };
 
 const getCardsData = (quantity) => {
   const data = [];
 
   for (let i = 0; i < quantity; i++) {
-    const isEdit = 0;
-    const isCreate = 0;
     const isRepeat = getRandomBool();
     const isFavorite = getRandomBool();
     const isArchive = getRandomBool();
 
-    const description = isCreate ? `` : getRandomItem(DESCRIPTIONS);
-    const color = isCreate ? `` : getRandomItem(COLORS_NAMES);
+    const description = getRandomItem(DESCRIPTIONS);
+    const color = getRandomItem(COLORS_NAMES);
     const dueDate = isRepeat ? null : getRandomDate();
+    const weekDays = getWeekDays(isRepeat);
     let isDeadline = false;
-
     if (dueDate && dueDate < new Date()) {
       isDeadline = true;
-    }
-
-    let weekDays = getWeekDays();
-
-    if (isCreate || !isRepeat) {
-      weekDays = getWeekDays(false);
     }
 
     data.push({
@@ -63,7 +52,6 @@ const getCardsData = (quantity) => {
       isFavorite,
       isArchive,
       isDeadline,
-      isEdit,
     });
   }
 
